e370b67d061a1637960ed53c893989c184d70db5
hugo9442 <hugo9442@gmail.com>
2025-02-28 17:30:10 +0000

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+
+
 let pronoum = ['me', 'the', 'your', 'our'];
 let adj = ['new', 'fantastic', 'beautiful', 'amazing'];
 let noum = ['house', 'car', 'dog', 'game'];
+//si añadimos un nuevo array de extension de dominio no haría falta hacer un ciclo anidado por cada extension.
+// valdia con hacer solo un anidado.
 
-for(let i = 0; i < pronoum.length; i++) {
+let extension = ['.com', '.es']
+
+
+for (let i = 0; i < pronoum.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noum.length; k++) {
+            for (let h = 0; h < extension.length; h++) {
+                console.log('DOMAIN GENERATED: '+pronoum[i] + adj[j] + noum[k] + extension[h])
+            }
+        }
+    }
+}
+
+
+/*for(let i = 0; i < pronoum.length; i++) {
     for(let j = 0; j < adj.length; j++) {
         for(let k = 0; k < noum.length; k++) {
             console.log(pronoum[i] + adj[j] + noum[k] + '.com')
@@ -16,4 +34,4 @@ for(let i = 0; i < pronoum.length; i++) {
             console.log(pronoum[i] + adj[j] + noum[k] + '.es')
         }
     }
-}
+}*/
